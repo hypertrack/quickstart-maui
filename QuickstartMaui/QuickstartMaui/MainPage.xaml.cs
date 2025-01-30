057d5b08f43739cb580fe76a44f07cdcbe8bb2ef
Pavel Kuznetsov <pavel@hypertrack.io>
2025-01-30 23:52:39 +0200

    wip

--- QuickstartMaui/QuickstartMaui/MainPage.xaml.cs
+++ QuickstartMaui/QuickstartMaui/MainPage.xaml.cs
@@ -1,6 +1,12 @@
 ﻿namespace QuickstartMaui;
 
+#if ANDROID
 using Com.Hypertrack.Sdk.Android;
+#endif
+
+#if IOS
+// using UIKit;
+#endif
 
 public partial class MainPage : ContentPage
 {
@@ -10,19 +16,25 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 
+		#if ANDROID
 		DeviceIdLabel.Text = HyperTrack.DeviceID;
+		#endif
+
+		#if IOS
+		DeviceIdLabel.Text = "iOS Device ID";
+		#endif
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+	// private void OnCounterClicked(object sender, EventArgs e)
+	// {
+	// 	count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+	// 	if (count == 1)
+	// 		CounterBtn.Text = $"Clicked {count} time";
+	// 	else
+	// 		CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+	// 	SemanticScreenReader.Announce(CounterBtn.Text);
+	// }
 }
 
